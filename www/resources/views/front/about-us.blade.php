aa27005ee35066fd674c31b7a4e2baed36c91492
dhwani-shah <dhwani.shah@tiez.nl>
2021-05-11 19:04:12 +0530

    Responsive Design

--- www/resources/views/front/about-us.blade.php
+++ www/resources/views/front/about-us.blade.php
@@ -5,7 +5,7 @@
   <div class="container">
     <div class="about_description">
       <div class="row">
-        <div class="col-md-6 col-lg-6">
+        <div class="col-md-12 col-lg-6">
             <div class="about_left">
               <h3>Mission & Vission</h3>
               <p>Our customers need and deserve high performance products, and the finest of raw materials and services. Our visionary goal is to continue to cater to them with honesty, integrity, and transparency, but also do so with commitment to providing quality and cost-effective products. Sustainability is also a core part of our policies as a nutraceutical chemical manufacturer, along with the aim of mastering operational excellence. Our mission and vision convene to curate services that allow our customers to meet the challenges of today and all the tomorrows.</p>
@@ -19,10 +19,10 @@
 <section class="world_sec">
   <div class="container">
     <div class="row">
-      <div class="col-md-6 col-lg-3">
+      <div class="col-md-12 col-lg-3">
         <img src="assets/images/world.png" alt="World" title="World" />
       </div>
-      <div class="col-md-6 col-lg-9">
+      <div class="col-md-12 col-lg-9">
         <h3>We help the world move forward</h3>
         <p>The world can become a better place when it is healthier and consumes the goodness the planet has to offer. At Nutracare, we are on a perpetual mission to contribute to a world that is healthy and hence, happy and hearty. By bringing you closer to nutrients, we like to call ourselves the helping hand in churning the wheel of change.</p>
       </div>
@@ -33,7 +33,7 @@
 <section class="value_block">
   <div class="container">
     <div class="row">
-      <div class="col-md-6 col-lg-3">
+      <div class="col-md-12 col-lg-3">
         <div class="value_left">
           <h3>Our Values</h3>
           <div class="value_img">
@@ -41,7 +41,7 @@
           </div>
         </div>
       </div>
-      <div class="col-md-6 col-lg-9">
+      <div class="col-md-12 col-lg-9">
         <div class="value_right">
           <div class="value_box">
             <h4>A Chemical Approach to Health</h4>

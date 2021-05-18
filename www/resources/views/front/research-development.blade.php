f921611487fa8c5f6bdce5b6d7b14954da785ca1
dhwani-shah <dhwani.shah@tiez.nl>
2021-05-18 18:19:26 +0530

    Research & Development Page

--- www/resources/views/front/research-development.blade.php
+++ www/resources/views/front/research-development.blade.php
@@ -4,25 +4,25 @@
 <section class="innovation_sec">
   <div class="container">
     <div class="row innovation_main">
-      <div class="col-md-12 col-lg-5">
+      <div class="col-md-12 col-lg-6 innovation_text">
         <div class="innovation_left">
           <h3>Research, Research, <br/> More Research</h3>
           <p>As an attempt to contribute to our mission and steer towards our vision, we at Nutracare International consider research and development as an integral and crucial part of our work. In our commitment to deliver quality and value for money as chemical manufacturers in India, we make R&D the pedestal of our products. Supported by a resourceful library and information management service, our efforts in research and development run majorly on innovation too.</p>
         </div>
       </div>
-      <div class="col-md-12 col-lg-7 text-center">
+      <div class="col-md-12 col-lg-6 text-center">
         <div class="innovation_right">
           <img src="assets/images/research.jpg" alt="Research" title="Research" />
         </div>
       </div>
     </div>
     <div class="row innovation_main">
-      <div class="col-md-12 col-lg-7 text-center div_order">
+      <div class="col-md-12 col-lg-6 text-center div_order">
         <div class="innovation_right">
           <img src="assets/images/quality.jpg" alt="Quality" title="Quality" />
         </div>
       </div>
-      <div class="col-md-12 col-lg-5">
+      <div class="col-md-12 col-lg-6 innovation_text">
         <div class="innovation_left">
           <h3>Quality is the Work of Patience</h3>
           <p>The purpose of delivering supreme quality stands at the core of the company. We believe in striving for excellence but with patience and mindfulness. We prefer not to cut short the chase and process to development. Instead, our R&D is focused on innovatory work ethics, ones that promise quality and conscious products.</p>
@@ -30,13 +30,13 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-md-12 col-lg-5">
-        <div class="innovation_left">
+      <div class="col-md-12 col-lg-6 innovation_text">
+        <div class="innovation_left no-padding">
           <h3>Equipped with the Resources</h3>
           <p>The in-house labs and equipment tools installed at Nutracare ensure an independent, full-fledged, and quality research and development. Our products are backed by innovation and the right resources to develop efficient and cost-effective technologies and manufacturing processes.</p>
         </div>
       </div>
-      <div class="col-md-12 col-lg-7 text-center">
+      <div class="col-md-12 col-lg-6 text-center">
         <div class="innovation_right">
           <img src="assets/images/resource.jpg" alt="resource" title="resource" />
         </div>

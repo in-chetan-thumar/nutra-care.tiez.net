b3c9d186f10aba5238642b8a3081139b18117b12
dhwani-shah <dhwani.shah@tiez.nl>
2021-05-18 18:46:16 +0530

    About Us page

--- www/resources/views/front/about-us.blade.php
+++ www/resources/views/front/about-us.blade.php
@@ -7,7 +7,7 @@
       <div class="row">
         <div class="col-md-12 col-lg-6">
             <div class="about_left">
-              <h3>Mission & Vission</h3>
+              <h3>Mission & Vision</h3>
               <p>Our customers need and deserve high performance products, and the finest of raw materials and services. Our visionary goal is to continue to cater to them with honesty, integrity, and transparency, but also do so with commitment to providing quality and cost-effective products. Sustainability is also a core part of our policies as a nutraceutical chemical manufacturer, along with the aim of mastering operational excellence. Our mission and vision convene to curate services that allow our customers to meet the challenges of today and all the tomorrows.</p>
             </div>
         </div>
@@ -35,7 +35,7 @@
     <div class="row">
       <div class="col-md-12 col-lg-3">
         <div class="value_left">
-          <h3>Our Values</h3>
+          <h3>Our <br/> Values</h3>
           <div class="value_img">
             <img src="assets/images/value.jpg" alt="Value" title="Value" />
           </div>
@@ -44,19 +44,19 @@
       <div class="col-md-12 col-lg-9">
         <div class="value_right">
           <div class="value_box">
-            <h4>A Chemical Approach to Health</h4>
+            <h4>A Chemical Approach <br/> to Health</h4>
             <p>A core purpose at Nutracare, we believe in the introduction of chemistry to health, and its uplifting results. Our value lies in customer satisfaction and catering to their needs of nutritional elements with the correct healthy chemistry formulation and techniques.</p>
           </div>
           <div class="value_box">
-            <h4>Keeping Up with Your Needs & Demands</h4>
+            <h4>Keeping Up with Your Needs <br/> & Demands</h4>
             <p>As the days and years pass, our needs and demands keep changing. Today, the world needs personal attention and made-to-order services. We are committed to working hard enough to be able to keep up with the needs and demands of our customers with efficient and cost-effective products and services.</p>
           </div>
           <div class="value_box">
-            <h4>Excelling but with Integrity and Honesty</h4>
+            <h4>Excelling but with Integrity <br/> and Honesty</h4>
             <p>We believe that we run on a fair dose of integrity, honesty, and perseverance. We not only believe in a sustainable future for the world but also sustainability in our work, as givers to society today. By maintaining transparency through our processes and products, we also hope to send out an inspiring message.</p>
           </div>
           <div class="value_box">
-            <h4>Flexibility and Quality as the Crux</h4>
+            <h4>Flexibility and Quality as <br/> the Crux</h4>
             <p>With our customers guiding our growth, we are constantly motivated to bring innovation into our work. With an open mind to feedback and customer inputs, we trust our flexibility and innovation to keep churning out high-quality products.</p>
           </div>
         </div>

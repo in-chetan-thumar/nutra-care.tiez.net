97e161bd3896ba71ec6375b9e8e64e2c58a68006
dhwani-shah <dhwani.shah@tiez.nl>
2021-05-18 18:59:51 +0530

    Responsive Design

--- www/resources/views/front/about-us.blade.php
+++ www/resources/views/front/about-us.blade.php
@@ -35,7 +35,7 @@
     <div class="row">
       <div class="col-md-12 col-lg-3">
         <div class="value_left">
-          <h3>Our <br/> Values</h3>
+          <h3>Our Values</h3>
           <div class="value_img">
             <img src="assets/images/value.jpg" alt="Value" title="Value" />
           </div>

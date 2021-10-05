cef49644e33452953645657721cc0dd7eaa5f1e2
Chetan Thumar <chetan.thumar@tiez.nl>
2021-10-05 17:01:33 +0530

    Change footer text

--- www/resources/lang/en/labels.php
+++ www/resources/lang/en/labels.php
@@ -39,7 +39,7 @@ return [
    'footer_email' => 'Email:',
    'footer_tel' => 'Tel:',
    'footer_fax' => 'Fax:',
-   'copyright_text' => 'Copyright © Nutra Care. All rights reserved',
+   'copyright_text' => 'Copyright © Nutracare International. All rights reserved',
 
    /*------------------------------------------- Contact Us Page ------------------------------------------- */
    /* Contact Us Details */

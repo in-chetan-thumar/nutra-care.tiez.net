1bebcb51ef2e626e466960b56e507299fd89dd3e
Kajal baldha <kajal.baldha@tiez.nl>
2023-09-20 18:29:22 +0530

    category

--- www/resources/views/admin/category/sub_category.blade.php
+++ www/resources/views/admin/category/sub_category.blade.php
@@ -2,11 +2,11 @@
 @foreach ($subcategories as $sub)
 
     <option value="{{ $sub->id }}">{{$prefix }} {{ $sub->title }}</option>
-    @if (count($sub->child) > 0)
+    @if (count($sub->childs) > 0)
         @php
             // Creating parents list separated by ->.
             $parents =  $sub->title;
         @endphp
-        @include('admin.category.sub_category', ['subcategories' => $sub->child, 'parent' => $parents,'prefix' => $prefix . ' - '])
+        @include('admin.category.sub_category', ['subcategories' => $sub->childs, 'parent' => $parents,'prefix' => $prefix . ' - '])
     @endif
 @endforeach
